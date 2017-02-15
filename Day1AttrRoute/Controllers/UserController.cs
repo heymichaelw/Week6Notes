3d46ddde1f7df033809b1b82b6c82a93af6084ec
heymichaelw <heymichaelw@gmail.com>
2017-02-15 10:30:51 -0500

    Notes

--- Day1AttrRoute/Controllers/UserController.cs
+++ Day1AttrRoute/Controllers/UserController.cs
@@ -18,7 +18,31 @@ namespace Day1AttrRoute.Controllers
         public ActionResult Detail(string userName)
         {
             ApplicationUser userInstance = db.Users.Where(u => u.UserName == userName).FirstOrDefault();
+            var me = User.Identity.GetUserId();
+            var target = userInstance.Id;
+            bool isFriend = db.Friends
+                .Where(f => (f.RequestorId == me && f.TargetId == target) ||
+                (f.TargetId == me && f.RequestorId == target))
+                .Any();
+            ViewBag.isFriend = isFriend;
             return View(userInstance);
+        }
+
+        [HttpPost]
+        [Route("u/{userName}")]
+        public ActionResult AddFriend(string userName)
+        {
+            var me = User.Identity.GetUserId();
+            var target = db.Users.Where(u => u.UserName == userName).FirstOrDefault().Id;
+            Friend relationship = new Friend
+            {
+                RequestorId = me,
+                TargetId = target
+            };
+            db.Friends.Add(relationship);
+            db.SaveChanges();
+
+            return RedirectToAction("Detail");
         }
     }
 }
